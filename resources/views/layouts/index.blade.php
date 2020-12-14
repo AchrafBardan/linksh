ec90de1f7c032c5eafa92496f0e3ab8f5cb94755
achraf <achrafbardan@gmail.com>
2020-12-14 23:17:59 +0100

    added pwa
    Web manifest and icons added

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="manifest" href="{{ asset('json/manifest.json') }}">
+    <link rel="apple-touch-icon" href="{{ asset('json/icon-512x512.png') }}">
+    <meta name="theme-color" content="#6366f1">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Link Shortner</title>
