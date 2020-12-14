4c90868823bda2827e520d49c4e2e887b0f340b7
achraf <achrafbardan@gmail.com>
2020-12-14 22:24:23 +0100

    test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
     <form class="flex flex-col gap-4" action="{{ route('newlink') }}" method="post">
         @csrf
         <input class="w-60  px-8 py-4 rounded-3xl border-indigo-200 border-b-4 placeholder-indigo-200 text-indigo-900 bg-indigo-300" value="https://" name="link" type="text" placeholder="Bv: https://google.com">
-        <button class="border-indigo-200 border-b-4 hover:bg-indigo-200 duration-300 hover:text-indigo-900 rounded-3xl py-4 px-8 bg-indigo-900 text-indigo-200" type="submit">Submit</button>
+        <button class="border-indigo-200 border-b-4 hover:bg-indigo-200 duration-300 hover:text-indigo-900 rounded-3xl py-4 px-8 bg-indigo-900 text-indigo-200" type="submit">Submitaren</button>
     </form>
 </div>
     
