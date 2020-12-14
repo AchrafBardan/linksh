b96987269a11788367fa5ed3f1f261f864f0eef9
achraf <achrafbardan@gmail.com>
2020-12-14 22:35:12 +0100

    added margins

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,9 +6,9 @@
                             Error
                         </div>
                     @enderror
-    <form class="flex flex-col gap-4" action="{{ route('newlink') }}" method="post">
+    <form class="flex flex-col" action="{{ route('newlink') }}" method="post">
         @csrf
-        <input class="w-60  px-8 py-4 rounded-3xl border-indigo-200 border-b-4 placeholder-indigo-200 text-indigo-900 bg-indigo-300" value="https://" name="link" type="text" placeholder="Bv: https://google.com">
+        <input class="w-60 mb-5 px-8 py-4 rounded-3xl border-indigo-200 border-b-4 placeholder-indigo-200 text-indigo-900 bg-indigo-300" value="https://" name="link" type="text" placeholder="Bv: https://google.com">
         <button class="border-indigo-200 border-b-4 hover:bg-indigo-200 duration-300 hover:text-indigo-900 rounded-3xl py-4 px-8 bg-indigo-900 text-indigo-200" type="submit">Submit</button>
     </form>
 </div>
--- resources/views/mylink.blade.php
+++ resources/views/mylink.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 <div class="w-full gap-4 h-screen flex flex-col items-center pt-20 bg-gradient-to-bl px-11 from-indigo-400 to-indigo-500">
-    <a href="{{ route('home') }}"><button class="px-4 py-2 bg-indigo-200 rounded-xl  shadow-inner">Terug</button></a>
+    <a href="{{ route('home') }}"><button class="px-4 py-2 mb-5 bg-indigo-200 rounded-xl  shadow-inner">Terug</button></a>
     @if ($req->link)
-        <div onclick="this.style.display = 'none';" class="px-10 py-4 text-center bg-green-500 rounded-2xl">
+        <div onclick="this.style.display = 'none';" class="px-10 py-4 mb-5 text-center bg-green-500 rounded-2xl">
             Url met de code {{ $req->link }} succesvol toegevoegd!
         </div>
     @endif
